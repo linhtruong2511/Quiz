8c9de4bfa9457ed374af972a35097fb40223f518
linht <linhtk2511044@gmail.com>
2025-01-04 22:39:16 +0700

    first config

--- src/main/java/com/Eddie/Quiz/QuizApplication.java
+++ src/main/java/com/Eddie/Quiz/QuizApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 	}
